2253fccabc109862247e6c6514ab0f97e908c0ce
bhuvan0501 <bhuvanmummadi05@gmail.com>
2025-06-06 15:16:52 +0530

    updated

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,15 +1,17 @@
 import express from "express";
 import userModel from '../models/userModel.js'; 
-
-
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken" ;
+const SECRET_KEY = "helloworld";
 const userRouter = express.Router();
 
 userRouter.post("/register", async (req, res) => {
   const { name, email, pass } = req.body;
+  const hashpassword = await bcrypt.hash(pass,10);
   const result = await userModel.insertOne({
     name: name,
     email: email,
-    pass: pass,
+    pass: hashpassword,
   });
   return res.json(result);
 });
